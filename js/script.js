32061d80d9e9228c95c33bfe3ae171408ffd4b52
Frederik Kristensen <frede4947@email.com>
2023-04-20 15:30:35 +0200

    Data types in html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,54 @@
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
 
-const first = 2;
-const secound = 3;
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
 
-const third = first + secound;
-console.log(third);
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        // this.currentOperand = number;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+        console.log(number);
+    }
+
+    // chooseOperation(operation) {
+        
+    // }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+    }
+
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
